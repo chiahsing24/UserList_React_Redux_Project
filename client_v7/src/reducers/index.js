41b4e727b2ca0a292a29b89eeefb765262bfe7f9
Chiahsing <42816092+chiahsing24@users.noreply.github.com>
2020-01-09 23:32:47 -0800

    Add files via upload
    Fix create functionality, no latency after clicking create button.

---
+++ client_v7/src/reducers/index.js
@@ -0,0 +1,27 @@
+import { combineReducers } from 'redux';
+import { reducer as formReducer} from 'redux-form';
+import usersReducer from './usersReducer';
+import currentPageReducer from './currentPageReducer';
+import setSearchReducer from './setSearchReducer';
+import editUserReducer from './editUserReducer';
+import fetchUserReducer from './fetchUserReducer';
+import setSortParamsReducer from './setSortParamsReducer';
+import updateTempUsersReducer from './updateTempUsersReducer';
+import setSortColumnReducer from './setSortColumnReducer';
+import setSearchForSortReducer from './setSearchForSortReducer';
+import setEditWhileSortReducer from './setEditWhileSortReducer';
+//import selectedUserToBeEditReducer from './selectedUserToBeEditReducer';
+
+export default combineReducers({
+    users: usersReducer,
+    form: formReducer,
+    currentPage: currentPageReducer,
+    searchQuery: setSearchReducer,
+    user: fetchUserReducer,
+    sortDirection: setSortParamsReducer,
+    tempUsers: updateTempUsersReducer,
+    sortColumn: setSortColumnReducer,
+    searchQueryForSort: setSearchForSortReducer,
+    isEditWhileSortAns: setEditWhileSortReducer
+    //selectedUserToBeEdit: selectedUserToBeEditReducer
+});
